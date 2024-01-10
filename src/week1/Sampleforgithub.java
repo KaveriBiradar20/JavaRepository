5aec7bbb7f01f6ba77d9df901b3243fec161dda6
KaveriBiradar <kaveribiradar178@gmail.com>
2024-01-10 06:24:03 +0530

    Second Commit

---
+++ src/week1/Sampleforgithub.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class Sampleforgithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This class is added to add in github repository");
+
+	}
+
+}
