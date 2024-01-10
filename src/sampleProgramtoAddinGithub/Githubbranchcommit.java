dcca20dcda9e968ccc307b09d9e3cabf276f528c
KaveriBiradar <kaveribiradar178@gmail.com>
2024-01-10 16:02:37 +0530

    commit for develop branch

---
+++ src/sampleProgramtoAddinGithub/Githubbranchcommit.java
@@ -0,0 +1,12 @@
+package sampleProgramtoAddinGithub;
+
+public class Githubbranchcommit {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Branch commit for github");
+
+	}
+
+}
